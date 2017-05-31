e7fe09ad1d04b98b0a034bbc35c447572e126bc9
yogesh <yogesh.d.barve@vanderbilt.edu>
2017-05-31 14:13:28 -0500

    update to java 8 installation

--- shell_build_scripts/bootstrap.sh
+++ shell_build_scripts/bootstrap.sh
@@ -374,16 +374,16 @@ java6_func(){
 java7_func(){
     sudo add-apt-repository ppa:webupd8team/java
     sudo apt-get update
-    sudo echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+    echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
     sudo apt-get install oracle-java7-installer -y
     sudo apt-get install oracle-java7-set-default -y
 }
 
 #This installs the java
 java8_func(){
-    sudo add-apt-repository ppa:webupd8team/java
+    sudo add-apt-repository ppa:webupd8team/java -y
     sudo apt-get update
-    sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+    echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
     sudo apt-get install oracle-java8-installer -y
     sudo apt-get install oracle-java8-set-default -y
 }
