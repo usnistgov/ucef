deb3e194e42219a716552097f952adaffe67a3ee
Himanshu Neema <himanshu.neema@vanderbilt.edu>
2017-06-02 13:36:16 -0500

    Added 'sudo sysctl -p' to disabling IPv6 updates for the updated /etc/sysctl.conf to take effect

--- shell_build_scripts/bootstrap.sh
+++ shell_build_scripts/bootstrap.sh
@@ -15,6 +15,7 @@ disable_ipv6() {
 	sudo echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 	sudo echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 	sudo echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+	sudo sysctl -p
 }
 
 init_func() {
