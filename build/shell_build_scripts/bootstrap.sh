4fb101eabee323b6a18179423e494d87ddd75efc
MartyBurns <marty@hypertek.us>
2018-03-15 15:30:01 -0400

    still trying to get mongod right

--- build/shell_build_scripts/bootstrap.sh
+++ build/shell_build_scripts/bootstrap.sh
@@ -96,9 +96,9 @@ mongodb_func(){
 #    sudo apt-get install -y mongodb-org
 
     # for Ubuntu 16.04
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-    echo "deb http://http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-    #sudo apt-get update
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+    sudo apt-get update
     sudo apt-get install -y -f mongodb-org
 
     sudo cp /home/vagrant/cpswt/cpswt-devtools/config/mongod.service /etc/systemd/mongod.service
