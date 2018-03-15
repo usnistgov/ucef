553977034fd8a307deffa86a6c35e19e54d1d4b4
MartyBurns <marty@hypertek.us>
2018-03-15 10:07:14 -0400

    update mongod install to xenial version

--- build/shell_build_scripts/bootstrap.sh
+++ build/shell_build_scripts/bootstrap.sh
@@ -97,7 +97,7 @@ mongodb_func(){
 
     # for Ubuntu 16.04
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-    echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+    echo "deb http://http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
     #sudo apt-get update
     sudo apt-get install -y -f mongodb-org
 
