368359635c038341e7a3ca7792eca2d6f05623b9
Thomas Roth <thomas.roth@nist.gov>
2020-04-29 15:57:04 -0400

    moved xrdp and ipv6 disable

--- packer/ucef/scripts/desktop.sh
+++ packer/ucef/scripts/desktop.sh
@@ -1,13 +1,54 @@
 #!/bin/bash -eux
 
 # install minimal desktop
-# apt-get install -y ubuntu-desktop firefox --no-install-recommends
 apt-get install -y xfce4 firefox # looking for something that works with Hyper-V
+apt-get install -y lightdm-gtk-greeter # avoid the default unity-greeter
+apt-get install -y lightdm
 
-# disable IPv6 at boot
-# GRUB_CMDLINE_LINUX will (sadly) contain an extra space; i do not know how to remove it
-sed -i 's/^GRUB_CMDLINE_LINUX="\(.*\)"$/GRUB_CMDLINE_LINUX="\1 ipv6.disable=1"/g' /etc/default/grub
-sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"$/GRUB_CMDLINE_LINUX_DEFAULT="\1 ipv6.disable=1"/g' /etc/default/grub
-update-grub
+# install xrdp (for Windows Remote Desktop)
+case "$PACKER_BUILDER_TYPE" in
+hyperv-iso)
+    # get the dependencies to build xrdp from source
+    apt-get install -y gcc make autoconf automake libtool pkgconf nasm
+    apt-get install -y xserver-xorg-dev libssl-dev libpam0g-dev libxfixes-dev libxrandr-dev
+    
+    # download and extract xrdp to /opt/xrdp-0.9.12
+    wget https://github.com/neutrinolabs/xrdp/releases/download/v0.9.12/xrdp-0.9.12.tar.gz
+    tar xvfz xrdp-0.9.12.tar.gz -C /opt
+    rm xrdp-0.9.12.tar.gz
+    
+    # compile xrdp
+    cd /opt/xrdp-0.9.12
+    ./bootstrap
+    ./configure
+    make
+    
+    # install xrdp
+    make install
+    ln -s /usr/local/sbin/xrdp /usr/sbin
+    ln -s /usr/local/sbin/xrdp-sesman /usr/sbin
+    
+    # download and extract xorgxrdp to /opt/xorgxrdp-0.2.12
+    wget https://github.com/neutrinolabs/xorgxrdp/releases/download/v0.2.12/xorgxrdp-0.2.12.tar.gz
+    tar xvfz xorgxrdp-0.2.12.tar.gz -C /opt
+    rm xorgxrdp-0.2.12.tar.gz
+    
+    # compile xorgxrdp
+    cd /opt/xorgxrdp-0.2.12
+    ./bootstrap
+    ./configure
+    make
+    
+    # install xorgxrdp
+    make install
+    
+    # fix ownership
+    chown -R vagrant:vagrant /opt/xrdp-0.9.12
+    chown -R vagrant:vagrant /opt/xorgxrdp-0.2.12
+    sed -i 's,allowed_users=console,allowed_users=anybody,g' /etc/X11/Xwrapper.config
+    
+    # configure xrdp
+    sudo systemctl enable xrdp
+esac
 
 reboot
--- packer/ucef/scripts/networking.sh
+++ packer/ucef/scripts/networking.sh
@@ -2,7 +2,7 @@
 
 # disable IPv6 at boot
 # GRUB_CMDLINE_LINUX will (sadly) contain an extra space; i do not know how to remove it
-# IPv6 must be disabled after xrdp is installed to prevent errors; networking.sh must occur after essential.sh
+# this can cause dns problems on some computers if done before the packages are installed ?
 sed -i 's/^GRUB_CMDLINE_LINUX="\(.*\)"$/GRUB_CMDLINE_LINUX="\1 ipv6.disable=1"/g' /etc/default/grub
 sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"$/GRUB_CMDLINE_LINUX_DEFAULT="\1 ipv6.disable=1"/g' /etc/default/grub
 update-grub
